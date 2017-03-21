f9cf878479006a001baeaf8ff7711686f2f53ee1
Aaron Nguyen and Max Reichman <aaron+max@>
2017-03-21 09:41:09 -0700

    Add more jQuery effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
+  $("h1").fadeIn(2000);
   $(".clickable").click(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
+  });
+  $(".fadeout").hover(function() {
+    $( this ).fadeOut(3000);
+  });
+  $("button").click(function() {
+    $(".intro").fadeToggle("slow", "linear");
   });
 });
